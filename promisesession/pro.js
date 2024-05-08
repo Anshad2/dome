bc2894ce66df6018f7e8513808e92f89946f8386
Anshad <anshadthorappa@gmail.com>
2024-05-08 08:02:26 +0530

    chnages

---
+++ promisesession/pro.js
@@ -0,0 +1,23 @@
+
+
+
+var lottery = new Promise((res,rej)=>{
+
+    let ticketNumber=5;
+
+    let priceNumber=Math.floor(Math.random()*10)
+
+    if(ticketNumber==priceNumber){
+
+        res("won!!!")
+    }
+
+})
+
+lottery.then(res=>console.log("buy a new car"))
+
+
+// api call methods
+//XMLHTTP
+// fetch()
+//axios()
